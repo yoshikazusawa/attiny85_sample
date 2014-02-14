081aca0cda2b5670852d60265229d1307316a5a5
Yoshikazu Sawa <yoshikazu.sawa@gmail.com>
2014-02-14 23:28:59 +0900

    write night status towards SoftwareSerial

--- attiny85_led_test.ino
+++ attiny85_led_test.ino
@@ -1,14 +1,19 @@
+#include <SoftwareSerial.h>
 
 const int CDS_SENSORPIN = A1;
 const int NIGHT_LEDPIN = 3;
 const int BLINK_LEDPIN = 4;
 
+const int RXPIN  = 0;
+const int TXPIN  = 1;
+SoftwareSerial softSerial(RXPIN, TXPIN);
 
 void setup()
 {
   pinMode(NIGHT_LEDPIN, OUTPUT);
   pinMode(BLINK_LEDPIN, OUTPUT);
   pinMode(CDS_SENSORPIN, INPUT);
+  softSerial.begin(9600);
 }
 
 void loop()
@@ -21,6 +26,9 @@ void radiate_in_night() {
   int value = analogRead(CDS_SENSORPIN);
   int is_night = value >= 512;
   digitalWrite(NIGHT_LEDPIN, is_night ? HIGH : LOW);
+  if (softSerial.available()) {
+    softSerial.write(is_night ? '1' : '0');
+  }
 }
 
 void blink_led() {
