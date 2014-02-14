a095537393a72e92ddfa82e9c21d59e59acee1ce
Yoshikazu Sawa <yoshikazu.sawa@gmail.com>
2014-02-14 23:28:49 +0900

    add night led

--- attiny85_led_test.ino
+++ attiny85_led_test.ino
@@ -1,14 +1,31 @@
-const int LEDPIN = 4;
+
+const int CDS_SENSORPIN = A1;
+const int NIGHT_LEDPIN = 3;
+const int BLINK_LEDPIN = 4;
+
 
 void setup()
 {
-  pinMode(LEDPIN, OUTPUT);
+  pinMode(NIGHT_LEDPIN, OUTPUT);
+  pinMode(BLINK_LEDPIN, OUTPUT);
+  pinMode(CDS_SENSORPIN, INPUT);
 }
 
 void loop()
 {
-  writePin(LEDPIN, HIGH, 1000);
-  writePin(LEDPIN, LOW, 1000);
+  radiate_in_night();
+  blink_led();
+}
+
+void radiate_in_night() {
+  int value = analogRead(CDS_SENSORPIN);
+  int is_night = value >= 512;
+  digitalWrite(NIGHT_LEDPIN, is_night ? HIGH : LOW);
+}
+
+void blink_led() {
+  writePin(BLINK_LEDPIN, HIGH, 200);
+  writePin(BLINK_LEDPIN, LOW, 200);
 }
 
 void writePin(int pin, int mode, int msec)
